3485e51e2980b0d26c56290f0860250d0c9722a6
jayasree <jayasrigandi18@gmail.com>
2025-05-23 14:54:29 +0530

    Initial Commit

---
+++ AnimalsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalsApp
+//
+//  Created by Jayasri Gandi on 23/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
